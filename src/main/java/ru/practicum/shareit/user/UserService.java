1c41baf9234f54dd32077b21a8512bba4850f6a9
MaximLunyov <Mr.Minnimi@yandex.com>
2023-08-08 12:22:33 +0300

    feat: "Добавлены реализации классов Item, User и их контроллеры"

--- src/main/java/ru/practicum/shareit/user/UserService.java
+++ src/main/java/ru/practicum/shareit/user/UserService.java
@@ -3,7 +3,7 @@ package ru.practicum.shareit.user;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import ru.practicum.shareit.item.model.UserMapper;
+import ru.practicum.shareit.user.dto.UserMapper;
 import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.model.User;
 
--- src/main/java/ru/practicum/shareit/user/dto/UserMapper.java
+++ src/main/java/ru/practicum/shareit/user/dto/UserMapper.java
@@ -1,4 +1,4 @@
-package ru.practicum.shareit.item.model;
+package ru.practicum.shareit.user.dto;
 
 import org.springframework.stereotype.Service;
 import ru.practicum.shareit.user.dto.UserDto;
